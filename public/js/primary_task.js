49ec73e3ffdf6b312a529073807a836772c8bff1
Jean Marc Touma <jatouma@JATOUMA-MBP.WIFI.WPI.EDU>
2016-05-12 03:58:40 -0400

    Change primary task words

--- public/js/primary_task.js
+++ public/js/primary_task.js
@@ -5,7 +5,7 @@
 //=====================GLOBALS=====================
   //Vars controlling what words are displayed
   //these could also be change to reflect an increase in difficulty
-  var words = easyWords = ["Turnt", "Fleek", "Fam", "Rekt", "Fuego", "Lit"];
+  var words = easyWords = ["Apple", "Avocadoe", "Apricot", "Asparagus", "Aubergine", "Arkansas"];
     var harderWords = ["HardTurnt", "HardFleek", "HardFam", "HardRekt", "HardFuego", "HardLit"];
 
   var rowSize = easyRowSize = 5;
