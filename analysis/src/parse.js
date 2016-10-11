b10309a39d424b5238e741732ecd90a40fc8a610
Chris <cjnavarro@wpi.edu>
2016-10-11 01:16:31 -0400

    Fixed weird parsing bug... hopefully we won't see any more

--- analysis/src/parse.js
+++ analysis/src/parse.js
@@ -3,10 +3,10 @@ var i = 0;
 
 console.log('{ "dataEntries" : [' );
 
-for(i = 0; i < data.length; i++){
+for(i = 0; i < data.length - 1; i++){
 	console.log(JSON.stringify(JSON.parse(data[i].data)));
 	
-	if(i < (data.length -1)) {
+	if(i < (data.length - 2)) {
 		console.log(",");
 	}
 }
