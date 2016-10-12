6bf6151974686f597c5437953eb7fe775fa2eff9
Chris <cjnavarro@wpi.edu>
2016-10-12 15:09:38 -0400

    fixed parser and cvs stuff

--- analysis/src/parse.js
+++ analysis/src/parse.js
@@ -4,11 +4,18 @@ var i = 0;
 console.log('{ "dataEntries" : [' );
 
 for(i = 0; i < data.length - 1; i++){
-	console.log(JSON.stringify(JSON.parse(data[i].data)));
-	
-	if(i < (data.length - 2)) {
-		console.log(",");
+
+	if (typeof data[i].data == 'undefined') {
+		while(typeof data[i].data == 'undefined') {
+			i++;
+		}
 	}
+		console.log(JSON.stringify(JSON.parse(data[i].data)));
+		
+		if(i < (data.length - 2)) {
+			console.log(",");
+		}
+	
 }
 
 console.log("]}");
