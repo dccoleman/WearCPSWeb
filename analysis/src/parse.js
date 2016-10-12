70c78a0df426b8ad18e5577f7f9bf1892aaeba97
Chris <cjnavarro@wpi.edu>
2016-10-12 15:11:54 -0400

    another fix

--- analysis/src/parse.js
+++ analysis/src/parse.js
@@ -3,7 +3,7 @@ var i = 0;
 
 console.log('{ "dataEntries" : [' );
 
-for(i = 0; i < data.length - 1; i++){
+for(i = 0; i < data.length; i++){
 
 	if (typeof data[i].data == 'undefined') {
 		while(typeof data[i].data == 'undefined') {
@@ -12,7 +12,7 @@ for(i = 0; i < data.length - 1; i++){
 	}
 		console.log(JSON.stringify(JSON.parse(data[i].data)));
 		
-		if(i < (data.length - 2)) {
+		if(i < (data.length-1)) {
 			console.log(",");
 		}
 	
